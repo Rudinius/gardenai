84a1e2d57f15ea7bcd9e6d61403fe99b6b5ab9f0
Mirco Rudolph <mirco.rudolph@gmail.com>
2024-04-30 13:20:06 +0700

    moved the tools object in api_call to a seperate .json file

--- api_call.py
+++ api_call.py
@@ -1,4 +1,5 @@
 import os
+import json
 from dotenv import load_dotenv
 from openai import OpenAI
 from openai import APIError
@@ -11,67 +12,9 @@ API_KEY = os.environ.get("GARDENAI_API_KEY", "If ENVVAR not set, paste API key h
 
 client = OpenAI(api_key=API_KEY)
 
-tools = [
-    {
-        "type": "function",
-        "function": {
-            "name": "retrieve_garden_info",
-            "description": """Retrieve the information provided by the user about the state,
-                the soil type, amount of sun in the garden, the preferred type of plant, 
-                and the preferred life-cycle of the plant""",
-            "parameters": {
-                "type": "object",
-                "properties": {
-                    "state": {
-                        "type": "string",
-                        "enum": ["", "AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", 
-                                 "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", 
-                                 "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", 
-                                 "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", 
-                                 "RI", "SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", 
-                                 "WI", "WY"],
-                        "description": """The state the user lives in. For example Michigan is MI. 
-                            Nothing mentioned is an empty string ''"""
-                    },
-                    "habit": {
-                        "type": "string",
-                        "enum": ["", "tree", "herb", "shrub", "grass/grass-like", 
-                                 "vine", "cactus/succulent", "fern"],
-                        "description": """The type of plant the user is looking for regarding the 
-                            the type of plant like tree or herb. 
-                            Nothing mentioned is an empty string ''""",
-                    },
-                    "duration": {
-                        "type": "string",
-                        "enum": ["", "perennial", "annual", "biennial"],
-                        "description": """The type of plant, the user is looking for regarding the
-                            the live duration of the plant. 
-                            Nothing mentioned is an empty string ''""",
-                    },
-                    "sun": {
-                        "type": "string",
-                        "enum": ["", "sun", "shade", "part-shade"],
-                        "description": """The amount of sun in the garden. For example 
-                            a sunny garden is sun. Nothing mentioned is an empty string ''"""
-                    },
-                    "water": {
-                        "type": "string",
-                        "enum": ["", "dry", "moist", "wet"],
-                        "description": """The soil type of the garden. For example 
-                            if the soil is dry, moist or wet. Nothing mentioned is an empty string ''"""
-                    },
-                    "other": {
-                        "type": "string",
-                        "description": """If the user provides any additional information
-                            for example if the user likes to have plant with fruits or
-                            a preference in color of the blossoms"""
-                    },
-                },
-                "required": ["state", "habit", "duration", "sun", "water"]
-            },
-        }
-    },
-]
+# Load the tools object from tools.json
+with open('tools.json', 'r') as tools:
+    tools = json.load(tools)
 
 def get_response_openai(chat_history, tool_choice="auto"):
     """
